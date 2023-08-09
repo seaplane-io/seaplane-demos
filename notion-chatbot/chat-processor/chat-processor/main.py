638f1b0cd55c2bb3907d11c354b4bd11e5fe7936
fokkedekker <fokke@seaplane.io>
2023-08-09 11:16:27 -0700

    adding chatbot demo

---
+++ notion-chatbot/chat-processor/chat-processor/main.py
@@ -0,0 +1,10 @@
+from notion_processor import process_notion
+from seaplane import app, start
+
+# the HTTP enabled application
+@app(id='processors', path='/process-chat-data', method=['POST', 'GET'])
+def chatbot_processor_application(data):
+	# call the notion processor
+    return process_notion(data)
+
+start()
