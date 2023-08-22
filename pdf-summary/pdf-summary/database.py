66f20b75049e7ddea97b041a2f348d378f779907
fokkedekker <fokke@seaplane.io>
2023-08-22 08:11:00 -0700

    removing json load no longer needed

--- pdf-summary/pdf-summary/database.py
+++ pdf-summary/pdf-summary/database.py
@@ -12,9 +12,6 @@ sql_access = {
 
 @task(type="sql", id="pdf-summary-db", sql=sql_access)
 def database(data, sql):
-    # convert response to JSON
-    data = json.loads(data)
-
     # get the summary from the response
     summary = data["choices"][0]["message"]["content"]
 
