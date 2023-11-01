f5ba6d99cc040515a474f2abe7b1dd7665a18ad3
Eshia Rustagi <eshia@seaplane.io>
2023-11-01 16:16:36 -0500

    Draft for updating hello-world demo

--- hello-world/hello-world/main.py
+++ hello-world/hello-world/main.py
@@ -1,26 +1,31 @@
 from seaplane import task, app, config, start
+import json
+
+# Configure secret credentials
 
 api_keys = {"SEAPLANE_API_KEY": "<YOUR-API-KEY>"}
 
 config.set_api_keys(api_keys)
 
 
+# World task
 @task(type="compute", id="world-task")
-def world(data):
-    s = "World " + data["name"]
-    return {"string": s}
+def world(context):
+    json_body = json.loads(context.body)
+    new_message = "World " + json_body['name']
+    context.emit(new_message)
 
+# Hello task
+@task(type='compute', id='hello-task')
+def hello(context):
+    world_output = context.body.decode()
+    final_message = "Hello " + world_output
+    context.emit(final_message)
 
-@task(type="compute", id="hello-task")
-def hello(data):
-    s = "hello " + data["string"]
-    return {"string": s}
-
-
-@app(path="/hello", method=["POST", "GET"], id="hello-world")
+# Application
+@app(path='/hello', method=['POST', 'GET'], id='hello-world')
 def hello_world(body):
     string = world(body)
     return hello(string)
-
 
 start()
