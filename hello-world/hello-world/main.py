4363562d8aedd2e28db29b3492469f874cc68681
Eshia Rustagi <eshia@seaplane.io>
2023-11-09 17:04:40 -0600

    Black format change

--- hello-world/hello-world/main.py
+++ hello-world/hello-world/main.py
@@ -1,24 +1,28 @@
 from seaplane import task, app, start
 import json
 
+
 # World task
 @task(type="compute", id="world-task")
 def world(context):
     json_body = json.loads(context.body)
-    new_message = "World " + json_body['name']
+    new_message = "World " + json_body["name"]
     context.emit(new_message)
 
+
 # Hello task
-@task(type='compute', id='hello-task')
+@task(type="compute", id="hello-task")
 def hello(context):
     world_output = context.body.decode()
     final_message = "Hello " + world_output
     context.emit(final_message)
 
+
 # Application
-@app(path='/hello', method=['POST', 'GET'], id='hello-world')
+@app(path="/hello", method=["POST", "GET"], id="hello-world")
 def hello_world(body):
     string = world(body)
     return hello(string)
+
 
 start()
